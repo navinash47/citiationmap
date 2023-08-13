bd898a1bdb185892098ac28700f6d4dc24ce2677
Avinash Nandyala <42831255+navinash47@users.noreply.github.com>
2023-08-13 10:10:29 +0000

    first commit

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/get_citations', methods=['GET'])
+def get_citations():
+    data = {
+        "nodes": [
+            {"id": "Paper A", "group": 1},
+            {"id": "Paper B", "group": 2},
+            {"id": "Paper C", "group": 2}
+        ],
+        "links": [
+            {"source": "Paper A", "target": "Paper B", "value": 1},
+            {"source": "Paper B", "target": "Paper C", "value": 1}
+        ]
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
